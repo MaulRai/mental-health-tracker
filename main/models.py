bf7e2a053fc39719ca9267d7d783d871af5e82ee
MaulRai <muhammadraihanofc@gmail.com>
2024-09-18 16:01:21 +0700

    push pws

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ import uuid  # tambahkan baris ini di paling atas
 from django.contrib.auth.models import User
 
 
+
 class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
